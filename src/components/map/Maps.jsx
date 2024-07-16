536e18bb061b6aa42ea04dbb1db7f00a82570cb1
Arome Jonathan Ukpoju <99257453+Tnath1@users.noreply.github.com>
2024-07-16 22:53:36 +0100

    map is out

--- src/components/map/Maps.jsx
+++ src/components/map/Maps.jsx
@@ -1,11 +1,36 @@
+// import React from "react";
+// import "./map.scss";
+// import "leaflet/dist/leaflet.css";
+
+// const Maps = () => {
+//   const position = [51.505, -0.09];
+//   return (
+//     <MapContainer center={position} zoom={13} scrollWheelZoom={false} className="map">
+//       <TileLayer
+//         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//       />
+//       <Marker position={position}>
+//         <Popup>
+//           A pretty CSS3 popup. <br /> Easily customizable.
+//         </Popup>
+//       </Marker>
+//     </MapContainer>
+//   );
+// };
+
+// export default Maps;
+
+
 import React from "react";
-import "./map.scss";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import "./map.scss";
 
 const Maps = () => {
   const position = [51.505, -0.09];
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false} className="map">
+    <MapContainer center={position} zoom={5} scrollWheelZoom={false} className="map">
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
