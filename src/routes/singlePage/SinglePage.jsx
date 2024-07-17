b3f15774cffcbc5bd0c3e883c7914d39c601d9d5
Arome Jonathan Ukpoju <99257453+Tnath1@users.noreply.github.com>
2024-07-17 13:39:25 +0100

    working on single page

--- src/routes/singlePage/SinglePage.jsx
+++ src/routes/singlePage/SinglePage.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import "./singlePage.scss"
+import React from "react";
+import "./singlePage.scss";
+import { CiLocationOn } from "react-icons/ci";
+import Slider from "../../components/slider/Slider";
 
+import { singlePostData, userData } from "../../lib/dummydata";
 const SinglePage = () => {
   return (
-    <div>SinglePage</div>
-  )
-}
+    <div className="singlePage">
+      <div className="details">
+        <div className="wrapper">
+          <Slider images={singlePostData.images} />
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>{singlePostData.title}</h1>
+                <div className="address">
+                  <CiLocationOn className="image" />
+                  <span>{singlePostData.address}</span>
+                </div>
+                <div className="price">$ {singlePostData.price}</div>
+              </div>
+              <div className="user">
+                <img src={userData.img} alt="" />
+                <span>{userData.name}</span>
+              </div>
+            </div>
+            <div className="bottom">
+              {singlePostData.description}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper"></div>
+      </div>
+    </div>
+  );
+};
 
-export default SinglePage
+export default SinglePage;
