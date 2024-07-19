a2e07b67c984b96609ca728ba3dd0413682ed110
Arome Jonathan Ukpoju <99257453+Tnath1@users.noreply.github.com>
2024-07-19 23:39:20 +0100

    working on profile page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ListPage from "./routes/listPage/ListPage";
 import Layout from "./routes/layout/Layout";
 import SinglePage from "./routes/singlePage/SinglePage";
+import ProfilePage from "./routes/profilePage/ProfilePage";
 
 function App() {
   const router = createBrowserRouter([
@@ -22,6 +23,10 @@ function App() {
         {
           path: "/:id",
           element: <SinglePage/>,
+        },
+        {
+          path: "/profile",
+          element: <ProfilePage/>,
         },
       ],
     },
